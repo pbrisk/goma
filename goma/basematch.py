a0b8ed4da975627af8f707e34988a651ee6ae0fa
sonntagsgesicht <sonntagsgesicht@icloud.com>
2019-09-16 22:58:32 +0200

    wrapped `eval` statement by `NotImplementedError`

--- goma/basematch.py
+++ goma/basematch.py
@@ -3,7 +3,7 @@
 # goma
 # ----
 # Generic object mapping algorithm.
-# 
+#
 # Author:   sonntagsgesicht, based on a fork of Deutsche Postbank [pbrisk]
 # Version:  0.2, copyright Saturday, 14 September 2019
 # Website:  https://github.com/sonntagsgesicht/goma
@@ -30,8 +30,8 @@ class BaseMatch(object):
             rel_vals = [type(detail_value)(val) for val in match_value[1:-1].split(',')]
             rel_ops = [self._repl_relation_operator(op) for op in [match_value[0], match_value[-1]]]
             # fixme NO eval usage !!!
-            match_res = eval(
-                'detail_value ' + rel_ops[0] + ' rel_vals[0] and detail_value ' + rel_ops[1] + ' rel_vals[1]')
+            cmd = 'detail_value ' + rel_ops[0] + ' rel_vals[0] and detail_value ' + rel_ops[1] + ' rel_vals[1]'
+            # match_res = eval(cmd)
         # else
         else:
             if self._repl_relation_operator(match_value[:2]):
@@ -41,9 +41,11 @@ class BaseMatch(object):
                 rel_ops = match_value[:1]
                 rel_val = type(detail_value)(match_value[1:])
 
-            match_res = eval('detail_value ' + rel_ops + ' rel_val')
-
-        return match_res
+            # fixme NO eval usage !!!
+            cmd = 'detail_value ' + rel_ops + ' rel_val'
+            # match_res = eval(cmd)
+        # return match_res
+        raise NotImplementedError('Relation match will be fixed in future releases.')
 
     def _repl_relation_operator(self, relation_symbol):
         if relation_symbol == '[': return '>='
